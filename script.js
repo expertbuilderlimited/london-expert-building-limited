9e4512ad52789097bf4b0ec7415b05d9938e3859
safi230r <115349427+safi230r@users.noreply.github.com>
2025-08-18 22:38:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openLightbox(src) {
+    document.getElementById('lightbox-img').src = src;
+    document.getElementById('lightbox').style.display = 'flex';
+}
+function closeLightbox() {
+    document.getElementById('lightbox').style.display = 'none';
+}
