0d528c8906ebff1a80dac8efa131a326d122ffb3
safi230r <115349427+safi230r@users.noreply.github.com>
2025-09-15 12:40:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+// Lightbox
 function openLightbox(src) {
-    document.getElementById('lightbox-img').src = src;
-    document.getElementById('lightbox').style.display = 'flex';
+  document.getElementById('lightbox-img').src = src;
+  document.getElementById('lightbox').style.display = 'flex';
 }
 function closeLightbox() {
-    document.getElementById('lightbox').style.display = 'none';
+  document.getElementById('lightbox').style.display = 'none';
+}
+
+// Mobile Menu Toggle
+function toggleMenu() {
+  document.getElementById('navLinks').classList.toggle('active');
 }
